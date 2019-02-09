90f5d1a8fb154ca953f466645ae4c73cc12b40cf
Alexander Mancevice <amancevice@cargometrics.com>
2019-02-09 13:05:59 -0500

    typo in Google Calendar link

--- lambda.js
+++ lambda.js
@@ -1,33 +1,30 @@
 'use strict';
-const AWS            = require('aws-sdk');
-const express        = require('aws-serverless-express');
+const AWS                  = require('aws-sdk');
+const awsServerlessExpress = require('aws-serverless-express');
+
+const AWS_SECRET = process.env.AWS_SECRET;
+
 const secretsmanager = new AWS.SecretsManager();
-const AWS_SECRET     = process.env.AWS_SECRET;
 
 let server;
 
-const createServer = (options) => {
-  console.log('CREATE SERVER');
-  return secretsmanager.getSecretValue(options).promise().then((res) => {
+const createServer = async (options) => {
+  // Get secret JSON
+  const secret = await secretsmanager.getSecretValue(options).promise();
 
-    // Update ENV
-    Object.assign(process.env, JSON.parse(res.SecretString));
+  // Update ENV
+  Object.assign(process.env, JSON.parse(secret.SecretString));
 
-    // Create server
-    server = express.createServer(require('./app'));
+  // Create server
+  server = awsServerlessExpress.createServer(require('./app'));
 
-    // Resolve server
-    return server;
-  });
+  // Resolve server
+  return server;
 }
 
-const getServer = (options) => {
-  // Use cached server on warm start or create on cold start
-  return Promise.resolve(server || createServer(options));
-};
-
-// Export Lambda handler
 exports.handler = (event, context) => {
   console.log(`EVENT ${JSON.stringify(event)}`);
-  getServer({SecretId: AWS_SECRET}).then((server) => express.proxy(server, event, context));
+  Promise.resolve(server || createServer({SecretId: AWS_SECRET})).then((server) => {
+    awsServerlessExpress.proxy(server, event, context);
+  });
 };
