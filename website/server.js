3a3358bcf77a0e6d5fa3ca40eda78603159da9a2
Alexander Mancevice <amancevice@cargometrics.com>
2019-07-02 17:26:40 -0400

    Add index.js entrypoint

--- website/server.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-require('dotenv').config();
-const app  = require('./app');
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`> Listening on port ${PORT}`));
