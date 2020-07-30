193897fb837dec2d18f1dd48e5421a5c9b150a80
Alexander Mancevice <alexander.mancevice@hey.com>
2020-07-30 15:53:44 -0400

    remove backend for secret

--- secrets/terraform.tf
+++ secrets/terraform.tf
@@ -1,77 +1,20 @@
-terraform {
-  backend s3 {
-    bucket  = "boston.techworkerscoalition.org"
-    key     = "terraform/website-secrets.tfstate"
-    region  = "us-east-1"
-    profile = "twc"
-  }
-
-  required_version = ">= 0.12.0"
-}
+terraform { required_version = "~> 0.12" }
 
 provider aws {
   region  = "us-east-1"
-  version = "~> 2.7"
+  version = "~> 2.70"
 }
 
-locals {
-  app  = "website"
-  repo = "https://github.com/techworkersco/twc-site-boston"
+variable GOOGLE_API_KEY { description = "Google API key" }
+variable GOOGLE_CALENDAR_ID { description = "Google Calendar ID" }
 
-  google_api_key     = var.google_api_key
-  google_calendar_id = var.google_calendar_id
-
-  secret = {
-    GOOGLE_API_KEY     = local.google_api_key
-    GOOGLE_CALENDAR_ID = local.google_calendar_id
-  }
-
-  tags = {
-    App     = local.app
-    Repo    = local.repo
-    Release = var.release
-  }
-}
-
-data aws_kms_key key {
-  key_id = var.kms_key_alias
-}
-
-resource aws_secretsmanager_secret secret {
-  description = "${local.app} secrets"
-  name        = local.app
-  kms_key_id  = data.aws_kms_key.key.id
-  tags        = local.tags
-}
+data aws_secretsmanager_secret secret { name = "website" }
 
 resource aws_secretsmanager_secret_version version {
-  secret_id     = aws_secretsmanager_secret.secret.id
-  secret_string = jsonencode(local.secret)
-}
+  secret_id = data.aws_secretsmanager_secret.secret.id
 
-variable kms_key_alias {
-  description = "KMS key alias."
-  default     = "alias/aws/secretsmanager"
-}
-
-variable google_api_key {
-  description = "Google API key."
-}
-
-variable google_calendar_id {
-  description = "Google Calendar ID."
-}
-
-variable release {
-  description = "Release tag."
-}
-
-output secret_arn {
-  description = "SecretsManager secret ARN."
-  value       = aws_secretsmanager_secret.secret.arn
-}
-
-output secret_name {
-  description = "SecretsManager secret name."
-  value       = aws_secretsmanager_secret.secret.name
+  secret_string = jsonencode({
+    GOOGLE_API_KEY     = var.GOOGLE_API_KEY
+    GOOGLE_CALENDAR_ID = var.GOOGLE_CALENDAR_ID
+  })
 }
